707682464931248a93ccbe1681d6a2b31c0099ce
Lucian <paunalucian14@gmail.com>
2019-01-18 12:16:12 +0000

    updated paths

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -7,7 +7,7 @@ gulp.task("sprite", function() {
     .pipe(spritesmith({
       imgName: "../app/assets/images/sprites/sprite.png",
       cssName: "../app/assets/styles/modules/_sprite.css",
-      imgPath: "../../../assets/images/sprites/sprite.png",
+      imgPath: "../../assets/images/sprites/sprite.png",
       padding: 2
     }));
     spriteData.img.pipe(gulp.dest("img"));
